65bbbefb8a1ae62fd566d5f53e761210565f6948
Bastien Guerry <bastien.guerry@data.gouv.fr>
2020-04-06 13:49:58 +0200

    Generate faq.json from latest csv

---
+++ src/clj/covid19faq/core.clj
@@ -0,0 +1,36 @@
+(ns covid19faq.core
+  (:require [cheshire.core :as json]
+            [clojure.data.csv :as csv]
+            [clojure.set]
+            [clojure.java.io :as io]))
+
+(def latest-csv-path "data/sources/2020.04.04_-_FAQ_gouvernement_V5.csv")
+
+(def csv
+  (with-open [reader (io/reader latest-csv-path)]
+    (doall
+     (csv/read-csv reader :separator \; :quote \"))))
+
+(defn rows->maps [csv]
+  (let [headers (butlast (first csv))
+        rows    (rest csv)]
+    (map #(zipmap headers %) rows)))
+
+(defn csv-as-map [csv] (rows->maps csv))
+
+(defn -main []
+  (spit "data/faq.json"
+        (json/generate-string
+         (map ;; identity
+          #(clojure.set/rename-keys
+            %
+            {"﻿Source"      :s
+             "URL"         :u
+             "Mise à jour" :m
+             "Catégorie"   :c
+             "Question"    :q
+             "Réponse"     :r})
+          (csv-as-map csv)))))
+
+;; (-main)
+
