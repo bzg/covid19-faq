19eb70acb8cfee895f450e14d64b1592362d7346
Bastien Guerry <bastien.guerry@data.gouv.fr>
2020-05-04 09:21:15 +0200

    Add stats

--- src/clj/covid19faq/core.clj
+++ src/clj/covid19faq/core.clj
@@ -78,6 +78,17 @@
      [:div.column.is-8.is-offset-2
       [:div {:id "app"}]]]
     [:script {:async true} "covid19faq.core.init();"]
+    [:script "(function(f, a, t, h, o, m){
+a[h]=a[h]||function(){
+(a[h].q=a[h].q||[]).push(arguments)
+};
+o=f.createElement('script'),
+m=f.getElementsByTagName('script')[0];
+o.async=1; o.src=t; o.id='fathom-script';
+m.parentNode.insertBefore(o,m)
+})(document, window, '//stats.bzg.fr/tracker.js', 'fathom');
+fathom('set', 'siteId', 'TRHXR');
+fathom('trackPageview');"]
     (footer)]))
 
 (defn -main []
